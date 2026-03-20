dc8eb9afc6d7c476de9094bd898b6f96da4706a4
Tuomas Yli-Korpela <e2402259@edu.vamk.fi>
2026-03-20 19:23:21 +0200

    Task 1.7 done. I removed BowlSelection component temporarily from App.tsx because it will be added in Task 1.13.

---
+++ web-app/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+    return (
+        <div className="bg-[#A2D135] text-black p-8 mt-12 w-full flex flex-wrap justify-around items-start">
+            <div>Contact</div>
+            <div>Social Media</div>
+            <div>Branding</div>
+        </div>
+    );
+}
