60d258a0d5f4564335ab0af84ba108996321fdf5
Tuomas Yli-Korpela <e2402259@edu.vamk.fi>
2026-03-20 19:44:02 +0200

    Task 1.13 done. It should start working when Task 1.10 has been done.

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,8 +1,22 @@
+import CenterBowl from "./components/CenterBowl";
+import BaseSelection from "./components/BaseSelection";
 import BowlSelection from "./components/BowlSelection";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 
 function App() {
   return (
-    <BowlSelection />
+    <div className="min-h-screen flex flex-col bg-white font-sans">
+      <Header />
+      <main className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4">
+        <div className="flex flex-col lg:flex-row gap-6 justify-between items-stretch">
+          <BowlSelection />
+          <CenterBowl />
+          <BaseSelection />
+        </div>
+      </main>
+      <Footer />
+    </div>
   )
 }
 export default App
