12e7ccae399c95fa4460578cbe977a1a63457215
Arttu <arttutikkamaki@gmail.com>
2026-03-27 09:50:07 +0200

    Created useStates for configurator.

--- web-app/src/pages/Configurator.tsx
+++ web-app/src/pages/Configurator.tsx
@@ -1,6 +1,11 @@
 import CenterBowl from "../components/CenterBowl";
+import { useState } from 'react';
+import type { Bowl } from '../types/index.ts';
 
 function Configurator() {
+    const [bowls, setBowls] = useState<Bowl[]>([]);
+    const [categories, setCategories] = useState<Bowl[]>([]);
+
     return (
         <div>
             <CenterBowl />
