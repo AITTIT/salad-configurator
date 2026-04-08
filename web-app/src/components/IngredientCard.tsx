4cf64edef41b7984ab1fa4e82832ccbbdd2c50d0
e2402230 <e2402230@edu.vamk.fi>
2026-04-08 12:40:25 +0300

    added onclick addIngredient

--- web-app/src/components/IngredientCard.tsx
+++ web-app/src/components/IngredientCard.tsx
@@ -1,12 +1,18 @@
 import type { Ingredient } from "../types";
+import { useIngredientStore } from "../store/useIngredientStore";
 
 interface IngredientCardProps {
   ingredient: Ingredient;
 }
 
 export default function IngredientCard({ ingredient }: IngredientCardProps) {
+  const addIngredient = useIngredientStore((state) => state.addIngredient);
+
   return (
-    <div className="w-40 h-40 p-4 border rounded-lg shadow-md flex flex-col justify-between bg-white">
+    <div
+    //currently has an empty placeholder body  so clicking won't do anything
+    onClick={() => addIngredient(ingredient)}
+    className="w-40 h-40 p-4 border rounded-lg shadow-md flex flex-col justify-between bg-white">
       <h3 className="text-lg font-semibold text-center text-black">{ingredient.name}</h3>
 
       <div className="flex justify-center gap-2 mt-2 flex-wrap">
